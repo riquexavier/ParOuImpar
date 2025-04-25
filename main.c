88c96b69c6bff09a503c83d1d8e3b6748a9de883
riquexavier <pedrooohhenriquex@hotmail.com>
2025-04-24 21:06:32 -0300

    em erro segundo beecr

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int qtd, n, i;
+    scanf("%i %i", &qtd, &n);
+
+    for ()
+    while(qtd
+    {
+        if (n == 0)
+        {
+            printf("NULL\n");
+        }
+        else if (n > 0)
+        {
+            printf("ODD POSITIVE\n");
+        }
+        else
+        {
+            printf("ODD NEGATITIVE\n");
+        }
+        scanf("%i", &n);
+    }
+    system("pause");
+    return 0;
+}
